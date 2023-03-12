0500804965ef19c5ca61c009ac7017dcd867dae5
castleiMac <castle@tansuyun.cn>
2023-03-12 19:29:28 +0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-碳素云 GoServer版本 ApiSDK
+碳素云 GoServer 版本 ApiSDK
 
-支持本地缓存，需要依赖服务端返回的数据md5值，实际内容为服务端计算的一个数据校验值
+支持本地缓存，需要依赖服务端返回的数据 md5 值，实际内容为服务端计算的一个数据校验值
+
+```html
+<script src="//npm.tansuyun.cn/buffer.js"></script>
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { crc8, store } from '@ctsy/common'
 import local from 'localforage'
 import hook, { HookWhen } from '@ctsy/hook'
+import { encode, decode } from "@msgpack/msgpack";
+// import "./buffer"
+
 export const SDKHooks = {
     BeforeRequest: 'BeforeRequest',
     AfterRequest: "AfterRequest"
@@ -10,10 +13,13 @@ var stores = local.createInstance({
     name: "ctsysdk"
 });
 var token = ""
+var Debug = true
 const req = axios.create({ withCredentials: true })
 
-req.interceptors.request.use(async (c: any) => {
-    let key = c.method + c.url + crc8(JSON.stringify(c.data || {}))
+
+//@ts-ignore
+req.interceptors.request.use(async (c: AxiosRequestConfig & { key: string }) => {
+    let key = [c.method, c.url, crc8(JSON.stringify(c.data || {}))].join('')
     c.key = key
     //读取并写入请求md5
     var cached = (await stores.getItem(key)) || { d: "", md5: "" }
@@ -25,24 +31,51 @@ req.interceptors.request.use(async (c: any) => {
         //@ts-ignore
         c.cdata = cached.d || ""
     }
+    //todo 增加请求体的md5值
+    //@ts-ignore
     c.headers.token = token
+    c.responseType = 'arraybuffer'
     await hook.emit(SDKHooks.BeforeRequest, HookWhen.Before, c, c)
+    if (!Debug && c.data && 'object' == typeof c.data) {
+        //检测是否支持pb，如果支持pb则使用pb编码，否则使用msgpack
+        c.data = encode(c.data)
+        //@ts-ignore
+        c.headers['Content-Type'] = 'application/msg'
+    }
     return c;
 })
 
 
-req.interceptors.response.use(async (r: any) => {
+req.interceptors.response.use(async (r) => {
     if (r.headers.token) {
         token = r.headers.token
         store.set("token", token)
     }
+    let content = r.headers['content-type']
+    if (r.status != 204 && r.data.byteLength > 0) {
+        if (content) {
+            if (content.includes('msg')) {
+                r.data = decode(r.data);
+            } else if (content.includes('proto') || content.includes('pb')) {
+                //TODO  r.data
+            } else if (content.includes('json')) {
+                //@ts-ignore
+                r.data = JSON.parse(Buffer.from(r.data).toString())
+            } else {
+                console.error("无法识别的响应内容")
+            }
+        }
+    }
+
     await hook.emit(SDKHooks.BeforeRequest, HookWhen.Before, r, r.data)
     // debugger
     if (r.headers.md5) {
         //存储缓存信息，
         if (r.status == 204) {
+            //@ts-ignore
             return r.config.cdata
         } else {
+            //@ts-ignore
             stores.setItem(r.config.key, { md5: r.headers.md5, d: r.data.d })
         }
     }
@@ -87,6 +120,10 @@ export function set_host(host: string) {
 }
 export function set_token(token: string) {
     token = token
+}
+
+export function set_debug(debug: boolean = true) {
+    Debug = debug
 }
 export class Request {
     Host = ""
